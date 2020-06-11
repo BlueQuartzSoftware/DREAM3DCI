15eee789fac3184298f59d01cf024dff8b51afb3
Jared Duffey <jared.duffey@bluequartz.net>
2020-06-11 17:58:04 -0400

    Updated to C++ 17
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- Repos.cmake
+++ Repos.cmake
@@ -21,40 +21,40 @@ set(CMP_GIT_BRANCH "develop")
 #--------------------------------------------------------------------------------------------------
 set(SIMPL_REPO_NAME "SIMPL")
 set(SIMPL_BASE_URL "https://www.github.com")
-set(SIMPL_GH_ORG "bluequartzsoftware")
-set(SIMPL_GIT_BRANCH "develop")
+set(SIMPL_GH_ORG "JDuffeyBQ")
+set(SIMPL_GIT_BRANCH "feature/cpp17")
 
 #--------------------------------------------------------------------------------------------------
 # SIMPLView Details
 #--------------------------------------------------------------------------------------------------
 set(SIMPLView_REPO_NAME "SIMPLView")
 set(SIMPLView_BASE_URL "https://www.github.com")
-set(SIMPLView_GH_ORG "bluequartzsoftware")
-set(SIMPLView_GIT_BRANCH "develop")
+set(SIMPLView_GH_ORG "JDuffeyBQ")
+set(SIMPLView_GIT_BRANCH "feature/cpp17")
 
 #--------------------------------------------------------------------------------------------------
 # H5Support Details
 #--------------------------------------------------------------------------------------------------
 set(H5Support_REPO_NAME "H5Support")
 set(H5Support_BASE_URL "https://www.github.com")
-set(H5Support_GH_ORG "bluequartzsoftware")
-set(H5Support_GIT_BRANCH "develop")
+set(H5Support_GH_ORG "JDuffeyBQ")
+set(H5Support_GIT_BRANCH "feature/cpp17")
 
 #--------------------------------------------------------------------------------------------------
 # EbsdLib Details
 #--------------------------------------------------------------------------------------------------
 set(EbsdLib_REPO_NAME "EbsdLib")
 set(EbsdLib_BASE_URL "https://www.github.com")
-set(EbsdLib_GH_ORG "bluequartzsoftware")
-set(EbsdLib_GIT_BRANCH "develop")
+set(EbsdLib_GH_ORG "JDuffeyBQ")
+set(EbsdLib_GIT_BRANCH "feature/cpp17")
 
 #--------------------------------------------------------------------------------------------------
 # DREAM3D Details
 #--------------------------------------------------------------------------------------------------
 set(DREAM3D_REPO_NAME "DREAM3D")
 set(DREAM3D_BASE_URL "https://www.github.com")
-set(DREAM3D_GH_ORG "bluequartzsoftware")
-set(DREAM3D_GIT_BRANCH "develop")
+set(DREAM3D_GH_ORG "JDuffeyBQ")
+set(DREAM3D_GIT_BRANCH "feature/cpp17")
 
 #--------------------------------------------------------------------------------------------------
 # DREAM3D_Data Details

44ef85b8972debc16d2ba70d7e1f995a66d49dd5
Michael Jackson <mike.jackson@bluequartz.net>
2020-06-18 18:30:39 -0400

    Remove Qt5 from EbsdLib
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Repos.cmake
+++ Repos.cmake
@@ -13,48 +13,48 @@ set(DREAM3D_Base_Repos
 #--------------------------------------------------------------------------------------------------
 set(CMP_REPO_NAME "CMP")
 set(CMP_BASE_URL "https://www.github.com")
-set(CMP_GH_ORG "bluequartzsoftware")
-set(CMP_GIT_BRANCH "develop")
+set(CMP_GH_ORG "imikejackson")
+set(CMP_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # SIMPL Details
 #--------------------------------------------------------------------------------------------------
 set(SIMPL_REPO_NAME "SIMPL")
 set(SIMPL_BASE_URL "https://www.github.com")
-set(SIMPL_GH_ORG "bluequartzsoftware")
-set(SIMPL_GIT_BRANCH "develop")
+set(SIMPL_GH_ORG "imikejackson")
+set(SIMPL_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # SIMPLView Details
 #--------------------------------------------------------------------------------------------------
 set(SIMPLView_REPO_NAME "SIMPLView")
 set(SIMPLView_BASE_URL "https://www.github.com")
-set(SIMPLView_GH_ORG "bluequartzsoftware")
-set(SIMPLView_GIT_BRANCH "develop")
+set(SIMPLView_GH_ORG "imikejackson")
+set(SIMPLView_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # H5Support Details
 #--------------------------------------------------------------------------------------------------
 set(H5Support_REPO_NAME "H5Support")
 set(H5Support_BASE_URL "https://www.github.com")
-set(H5Support_GH_ORG "bluequartzsoftware")
-set(H5Support_GIT_BRANCH "develop")
+set(H5Support_GH_ORG "imikejackson")
+set(H5Support_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # EbsdLib Details
 #--------------------------------------------------------------------------------------------------
 set(EbsdLib_REPO_NAME "EbsdLib")
 set(EbsdLib_BASE_URL "https://www.github.com")
-set(EbsdLib_GH_ORG "bluequartzsoftware")
-set(EbsdLib_GIT_BRANCH "develop")
+set(EbsdLib_GH_ORG "imikejackson")
+set(EbsdLib_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # DREAM3D Details
 #--------------------------------------------------------------------------------------------------
 set(DREAM3D_REPO_NAME "DREAM3D")
 set(DREAM3D_BASE_URL "https://www.github.com")
-set(DREAM3D_GH_ORG "bluequartzsoftware")
-set(DREAM3D_GIT_BRANCH "develop")
+set(DREAM3D_GH_ORG "imikejackson")
+set(DREAM3D_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # DREAM3D_Data Details
@@ -62,7 +62,7 @@ set(DREAM3D_GIT_BRANCH "develop")
 set(DREAM3D_Data_REPO_NAME "DREAM3D_Data")
 set(DREAM3D_Data_BASE_URL "https://www.github.com")
 set(DREAM3D_Data_GH_ORG "dream3d")
-set(DREAM3D_Data_GIT_BRANCH "develop")
+set(DREAM3D_Data_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 set(DREAM3D_Plugins
   ITKImageProcessing
@@ -76,16 +76,16 @@ set(DREAM3D_Plugins
 #--------------------------------------------------------------------------------------------------
 set(ITKImageProcessing_REPO_NAME "ITKImageProcessing")
 set(ITKImageProcessing_BASE_URL "https://www.github.com")
-set(ITKImageProcessing_GH_ORG "bluequartzsoftware")
-set(ITKImageProcessing_GIT_BRANCH "develop")
+set(ITKImageProcessing_GH_ORG "imikejackson")
+set(ITKImageProcessing_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # SimulationIO Details
 #--------------------------------------------------------------------------------------------------
 set(SimulationIO_REPO_NAME "SimulationIO")
 set(SimulationIO_BASE_URL "https://www.github.com")
-set(SimulationIO_GH_ORG "bluequartzsoftware")
-set(SimulationIO_GIT_BRANCH "develop")
+set(SimulationIO_GH_ORG "imikejackson")
+set(SimulationIO_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # DREAM3DReview Details
@@ -93,7 +93,7 @@ set(SimulationIO_GIT_BRANCH "develop")
 set(DREAM3DReview_REPO_NAME "DREAM3DReview")
 set(DREAM3DReview_BASE_URL "https://www.github.com")
 set(DREAM3DReview_GH_ORG "dream3d")
-set(DREAM3DReview_GIT_BRANCH "develop")
+set(DREAM3DReview_GIT_BRANCH "topic/ebsd_lib_remove_qt")
 
 #--------------------------------------------------------------------------------------------------
 # UCSBUtilities Details
@@ -101,4 +101,4 @@ set(DREAM3DReview_GIT_BRANCH "develop")
 set(UCSBUtilities_REPO_NAME "UCSBUtilities")
 set(UCSBUtilities_BASE_URL "https://www.github.com")
 set(UCSBUtilities_GH_ORG "dream3d")
-set(UCSBUtilities_GIT_BRANCH "develop")
+set(UCSBUtilities_GIT_BRANCH "topic/ebsd_lib_remove_qt")

637c39ff452e9bbe03be9cd5ee5b8ae5395b5c4e
Jared Duffey <jared.duffey@bluequartz.net>
2021-02-19 13:07:27 -0500

    Testing embedded Python branches
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- Repos.cmake
+++ Repos.cmake
@@ -24,16 +24,16 @@ set(CMP_GIT_BRANCH "develop")
 #--------------------------------------------------------------------------------------------------
 set(SIMPL_REPO_NAME "SIMPL")
 set(SIMPL_BASE_URL "https://www.github.com")
-set(SIMPL_GH_ORG "bluequartzsoftware")
-set(SIMPL_GIT_BRANCH "develop")
+set(SIMPL_GH_ORG "JDuffeyBQ")
+set(SIMPL_GIT_BRANCH "feature/embedded_python")
 
 #--------------------------------------------------------------------------------------------------
 # SIMPLView Details
 #--------------------------------------------------------------------------------------------------
 set(SIMPLView_REPO_NAME "SIMPLView")
 set(SIMPLView_BASE_URL "https://www.github.com")
-set(SIMPLView_GH_ORG "bluequartzsoftware")
-set(SIMPLView_GIT_BRANCH "develop")
+set(SIMPLView_GH_ORG "JDuffeyBQ")
+set(SIMPLView_GIT_BRANCH "feature/embedded_python")
 
 #--------------------------------------------------------------------------------------------------
 # H5Support Details
@@ -56,8 +56,8 @@ set(EbsdLib_GIT_BRANCH "develop")
 #--------------------------------------------------------------------------------------------------
 set(DREAM3D_REPO_NAME "DREAM3D")
 set(DREAM3D_BASE_URL "https://www.github.com")
-set(DREAM3D_GH_ORG "bluequartzsoftware")
-set(DREAM3D_GIT_BRANCH "develop")
+set(DREAM3D_GH_ORG "JDuffeyBQ")
+set(DREAM3D_GIT_BRANCH "feature/embedded_python")
 
 #--------------------------------------------------------------------------------------------------
 # DREAM3D_Data Details
